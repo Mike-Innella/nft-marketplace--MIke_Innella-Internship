79bad64655a10aa9954d6eea0bab0f9bbb3ff7f5
Michael Innella <mainnella@gmail.com>
2025-05-05 12:31:49 -0400

    Update TopSellers.jsx
    added same skeleton state and swiper

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import axios from "axios";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import SkeletonCard from "../UI/SkeletonCard";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
 const TopSellers = () => {
+  const [sellers, setSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTopSellers = async () => {
+      try {
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+        );
+        setSellers(response.data);
+        setTimeout(() => setLoading(false), 1200); // <-- delay loading flag
+      } catch (error) {
+        console.error("Failed to fetch top sellers:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchTopSellers();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -14,26 +43,58 @@ const TopSellers = () => {
             </div>
           </div>
           <div className="col-md-12">
-            <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
-            </ol>
+            <Swiper
+              spaceBetween={16}
+              slidesPerView={6}
+              navigation
+              pagination={{ clickable: true }}
+              modules={[Navigation, Pagination]}
+              breakpoints={{
+                0: { slidesPerView: 2 },
+                576: { slidesPerView: 3 },
+                768: { slidesPerView: 4 },
+                992: { slidesPerView: 5 },
+                1200: { slidesPerView: 6 },
+              }}
+            >
+              {loading
+                ? Array.from({ length: 12 }).map((_, index) => (
+                    <SwiperSlide key={index}>
+                      <SkeletonCard />
+                    </SwiperSlide>
+                  ))
+                : sellers.map((seller, index) => (
+                    <SwiperSlide key={index}>
+                      <div
+                        className="top-sellers__card"
+                        style={{
+                          height: "128px",
+                          display: "flex",
+                          flexDirection: "row",
+                          alignItems: "center",
+                          borderRadius: "12px",
+                        }}
+                      >
+                        <div className="author_list_pp">
+                          <Link to={`/author/${seller.authorId}`}>
+                            <img
+                              className="lazy pp-author"
+                              src={seller.authorImage || AuthorImage}
+                              alt={seller.authorName}
+                            />
+                            <i className="fa fa-check"></i>
+                          </Link>
+                        </div>
+                        <div className="author_list_info">
+                          <Link to={`/author/${seller.authorId}`}>
+                            {seller.authorName}
+                          </Link>
+                          <span>{seller.price} ETH</span>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                  ))}
+            </Swiper>
           </div>
         </div>
       </div>
